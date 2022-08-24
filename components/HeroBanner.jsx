f3ab8ff65e4427585679c578f662d2ef5ac79ce4
Annlee Fores <annleefores@gmail.com>
2022-08-24 23:52:31 +0530

    hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,15 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { urlFor } from "../lib/client";
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
 
-        <img src="" alt="headphones" className="hero-banner-image" />
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
 
         <div>
           <Link href="/product/ID">
