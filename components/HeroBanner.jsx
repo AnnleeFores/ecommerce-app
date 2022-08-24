3099313dd79ae48f3a7f08ba4d25937ddda066e2
Annlee Fores <annleefores@gmail.com>
2022-08-24 23:14:58 +0530

    setting up sanity client

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+
+        <img src="" alt="headphones" className="hero-banner-image" />
+
+        <div>
+          <Link href="/product/ID">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>description</h5>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
