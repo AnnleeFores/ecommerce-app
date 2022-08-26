36716b9a4736a3d96ecfbd9852a32271bf284d42
Annlee Fores <annleefores@gmail.com>
2022-08-26 19:52:48 +0530

    stripe setup

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -23,6 +23,8 @@ const Cart = () => {
     onRemove,
   } = useStateContext();
 
+  const handleCheckout = () => {};
+
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
@@ -108,7 +110,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
+              <button type="button" className="btn" onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
