32cb7b824dc7e79bf48128c81801a74eb549d143
Annlee Fores <annleefores@gmail.com>
2022-08-26 20:21:04 +0530

    passing product data to stripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -12,6 +12,8 @@ import toast from "react-hot-toast";
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
 
+import getStripe from "../lib/getStripe";
+
 const Cart = () => {
   const cartRef = useRef();
   const {
@@ -23,7 +25,26 @@ const Cart = () => {
     onRemove,
   } = useStateContext();
 
-  const handleCheckout = () => {};
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch("/api/stripe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+
+    toast.loading("redirecting...");
+    console.log("redirecting...");
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  };
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,6 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    console.log(req.body);
     try {
       const params = {
         submit_type: "pay",
@@ -11,13 +12,31 @@ export default async function handler(req, res) {
           { shipping_rate: "shr_1Lb3NbSFb0twFeWUpNd9lTaU" },
           { shipping_rate: "shr_1Lb3OYSFb0twFeWU0YpYUVW8" },
         ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/0tmo8axz/production/"
+            )
+            .replace("-webp", ".webp");
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
         mode: "payment",
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
