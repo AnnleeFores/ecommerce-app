d5a1ea1d845dab83002f6752fe01fba022666626
Annlee Fores <annleefores@gmail.com>
2022-08-26 19:54:08 +0530

    stripe setup

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,12 +1,35 @@
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-    } catch (error) {
-      res.status(500).json({ statusCode: 500, message: error.message });
+      const params = {
+        submit_type: "pay",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1Lb3NbSFb0twFeWUpNd9lTaU" },
+          { shipping_rate: "shr_1Lb3OYSFb0twFeWU0YpYUVW8" },
+        ],
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: "{{PRICE_ID}}",
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
     }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
   }
 }
