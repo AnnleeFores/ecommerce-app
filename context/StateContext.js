07ba1b2b32e5701eec3a6d5c11601e8c4dd16e5a
Annlee Fores <annleefores@gmail.com>
2022-08-26 15:09:25 +0530

    add to cart fixed -> toast not working

--- context/StateContext.js
+++ context/StateContext.js
@@ -4,11 +4,41 @@ import { toast } from "react-hot-toast";
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
-  const [showCart, setshowCart] = useState(false);
-  const [cartItems, setcartItems] = useState([]);
-  const [totalPrice, settotalPrice] = useState();
-  const [totalQuantities, settotalQuantities] = useState();
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          };
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product }]);
+    }
+
+    toast.success(`${qty} ${product.name} added to the cart.`);
+    console.log(`${qty} ${product.name} added to the cart.`);
+  };
 
   const incQty = () => {
     setQty((prevQty) => prevQty + 1);
@@ -22,39 +52,11 @@ export const StateContext = ({ children }) => {
     });
   };
 
-  const onAdd = (product, quantity) => {
-    const checkProductInCart = cartItems.find(
-      (item) => item._id === product._id
-    );
-
-    settotalPrice(
-      (prevTotalPrice) => prevTotalPrice + product.price * quantity
-    );
-    settotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
-
-    if (checkProductInCart) {
-      const updatedCartItems = cartItems.map((cartProduct) => {
-        if (cartProduct._id === product._id)
-          return {
-            ...cartProduct,
-            quantity: cartProduct.quantity + quantity,
-          };
-      });
-
-      setcartItems(updatedCartItems);
-    } else {
-      product.quantity = quantity;
-
-      setcartItems([...cartItems, { ...product }]);
-    }
-    toast.success(`${qty} ${product.name} added to the cart.`);
-  };
-
   return (
     <Context.Provider
       value={{
         showCart,
-        cartItem,
+        cartItems,
         totalPrice,
         totalQuantities,
         qty,
