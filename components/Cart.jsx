f50483d7e037eb1d3bf9afa264e9ebb27647e671
Annlee Fores <annleefores@gmail.com>
2022-08-26 15:25:29 +0530

    cart done

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -13,7 +13,25 @@ import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
+
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
