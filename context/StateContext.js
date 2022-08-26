88ded8ac27a9aaac29b21932d41ea5f865e1f6e8
Annlee Fores <annleefores@gmail.com>
2022-08-26 18:49:07 +0530

    cart complete

--- context/StateContext.js
+++ context/StateContext.js
@@ -44,13 +44,31 @@ export const StateContext = ({ children }) => {
     console.log(`${qty} ${product.name} added to the cart.`);
   };
 
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id);
+    const newCartItems = cartItems.filter((item) => item._id !== product._id);
+
+    setTotalPrice(
+      (prevTotalPrice) =>
+        prevTotalPrice - foundProduct.price * foundProduct.quantity
+    );
+
+    setTotalQuantities(
+      (prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity
+    );
+
+    setCartItems(newCartItems);
+  };
+
   const toggleCartItemQuantity = (id, value) => {
     foundProduct = cartItems.find((item) => item._id === id);
     index = cartItems.findIndex((product) => product._id === id);
 
+    const newCartItems = cartItems.filter((item) => item._id !== id);
+
     if (value === "inc") {
       setCartItems([
-        ...cartItems,
+        ...newCartItems,
         { ...foundProduct, quantity: foundProduct.quantity + 1 },
       ]);
       setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
@@ -91,6 +109,8 @@ export const StateContext = ({ children }) => {
         decQty,
         onAdd,
         setShowCart,
+        toggleCartItemQuantity,
+        onRemove,
       }}
     >
       {children}
